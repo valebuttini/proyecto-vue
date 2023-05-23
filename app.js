599ebf54ee885a0853d0074c1c52369283d4c37e
Valentin Buttini <buttiniprocer@hotmail.com>
2023-05-23 20:34:28 -0300

    termine el contador, agregue bootstrap y css

--- app.js
+++ app.js
@@ -2,15 +2,26 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            titulo: "hola mundo de vue",
-            parrafo: "esto es una app de gatos",
-            catalogo: "tenemos stock" 
+            titulo: "ESTO ES UNA APP DE COMIDA",
+            parrafo: "Nuestro catalogo sera de comidas tradicionales argentinas",
+            parrafoInformativo: "Estamos abriendo !! Hoy solo tenemos asado clasico !!!" ,
+            div:` 
+            <h1> ELEGI LA CANTIDAD DE PIEZAS</h1>
+            `,
+            numero: 1,
+            parrafo1: ""
         }
     },
     created(){
 
     },
     methods:{
+        suma(){
+            this.numero +=1
+        },
+        resta(){
+            this.numero -=1
+        }
 
     }
 }).mount("#app")
